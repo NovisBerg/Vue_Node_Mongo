c84007d10fb00aeecd6473ab189390c710879b1f
BergNovis <jacktwist.ennis@outlook.com>
2020-05-24 21:52:38 +0800

    Initial commit

---
+++ ImoocMall/src/components/Counter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button v-on:click="increment">
+            +
+        </button>
+        <button @click="decrement">
+            -
+        </button>
+        <p><span>{{ num }}</span></p>
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  props: ['num'],
+  data() {
+      return {
+
+      }
+  },
+  methods: {
+      increment() {
+        this.num++
+      },
+      decrement() {
+        this.num--
+      }
+  }
+}
+</script>
---
+++ ImoocMall/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <counter :num="num"></counter>
+    <p>parent: {{ num }}</p>
+  </div>
+</template>
+
+<script>
+  import Counter from './Counter'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      num: 10
+    }
+  },
+  components: {
+    Counter
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
